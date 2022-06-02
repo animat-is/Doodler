968db0926352dc2918752dc1cb925ad6188b0c05
punkyunicorn <58108013+punkyunicorn@users.noreply.github.com>
2022-06-02 19:38:49 +0200

    moved component classes to a separate folder

--- Plugins/PaintingSystem/Source/PaintingSystem/Private/Components/MeshPaintingComponent.cpp
+++ Plugins/PaintingSystem/Source/PaintingSystem/Private/Components/MeshPaintingComponent.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "MeshPaintingComponent.h"
+#include "Components/MeshPaintingComponent.h"
 
 #include "Engine/TextureRenderTarget2D.h"
 #include "PaintingSystemFunctionLibrary.h"
--- Plugins/PaintingSystem/Source/PaintingSystem/Private/Components/PaintingComponent.cpp
+++ Plugins/PaintingSystem/Source/PaintingSystem/Private/Components/PaintingComponent.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "PaintingComponent.h"
+#include "Components/PaintingComponent.h"
 #include "PaintingSubsystem.h"
 
 DEFINE_LOG_CATEGORY(LogPainting);
